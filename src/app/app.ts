21771c78970b3646c2fce883ddc162dce5d63a87
seesagar <seeesagar@gmail.com>
2026-07-29 01:09:10 +0530

    project name changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-firstapp');
+  protected readonly title = signal('thebyebyebox');
 }
