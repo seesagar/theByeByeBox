fa58bfcba3a87979742db6beb2ee47943c768e4c
seesagar <seeesagar@gmail.com>
2026-08-04 01:44:23 +0530

    About page changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup';
+import { AboutComponent } from './about/about';
 
 
 export const routes: Routes = [
   { path: '', redirectTo: '/signup', pathMatch: 'full' },
-  { path: 'signup', component: SignupComponent }
+  { path: 'signup', component: SignupComponent },
+  { path: 'about', component: AboutComponent }
 ];
